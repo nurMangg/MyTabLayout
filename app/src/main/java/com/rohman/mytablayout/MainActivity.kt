5f566aa23f3aa34ff65324ddf7d370c5b6aac5d1
nurmangg <rohmanuyeoke@gmail.com>
2024-03-21 20:33:28 +0700

    Update: TabLayout 3

--- app/src/main/java/com/rohman/mytablayout/MainActivity.kt
+++ app/src/main/java/com/rohman/mytablayout/MainActivity.kt
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         @StringRes
         private val TAB_TITLES = intArrayOf(
             R.string.tab_text,
-            R.string.tab_text_2
+            R.string.tab_text_2,
+            R.string.tab_text_3
         )
     }
 
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        sectionsPagerAdapter.appName = resources.getString(R.string.app_name)
         val viewPager: ViewPager2 = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
--- app/src/main/java/com/rohman/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/rohman/mytablayout/SectionsPagerAdapter.kt
@@ -1,21 +1,24 @@
 package com.rohman.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
+    var appName: String = "Halo Saya Seorang Programmer!"
     override fun getItemCount(): Int {
-        return 2;
+        return 3;
     }
 
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            putString(HomeFragment.ARG_NAME, appName)
         }
-        return fragment as Fragment
+        return fragment
     }
 
 
